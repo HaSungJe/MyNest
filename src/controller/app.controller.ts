1114af043da18e4a3cb064c982ad822b92f82385
하성제 <timeclas3@gamil.com>
2024-04-14 20:09:56 +0900

    2024.04.14
    - swagger 사용

--- src/controller/app.controller.ts
+++ src/controller/app.controller.ts
@@ -1,9 +1,11 @@
+import { ApiTags, ApiConsumes, ApiOperation, ApiResponse, ApiBody } from '@nestjs/swagger';
 import { Controller, Delete, Get, Put, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { AppService } from '../service/app.service';
 import { AWSService } from '../service/aws.service';
 import * as util from '../util/util';
 
+@ApiTags('API List')
 @Controller()
 export class AppController {
     constructor(
@@ -19,6 +21,39 @@ export class AppController {
     
     // 테스트
     @Get("/test")
+    @ApiConsumes('application/x-www-form-urlencoded')
+    @ApiOperation({
+        summary: 'test API summary',
+        description: 'test API description',
+    })
+    @ApiBody({
+        schema: {
+            type: 'object',
+            properties: {
+                data1: {type: 'string', description: '데이터 1', example: 'aaaa'},
+                data2: {type: 'string', description: '데이터 2', example: 'bbbb'},
+                data3: {type: 'string', description: '데이터 3', example: 'cccc'},
+                data4: {type: 'string', description: '데이터 4', example: 'dddd'},
+            },
+            required: ['data1', 'data2']
+        }
+    })
+    @ApiResponse({ status: 200, description: 'Success', schema: {
+        type: 'array',
+        properties: {
+            user_id: {type: 'number', description: '회원 아이디', example: 1},
+            gender: {type: 'number', description: '성별 코드', example: 0},
+            age: {type: 'number', description: '나이', example: 26},
+            joined: {type: 'string', description: '생성일', example: '2021-06-06'}
+        }
+    }})
+    @ApiResponse({ status: 400, description: 'Fail', schema: {
+        type: 'object',
+        properties: {
+            success: {type: 'number', description: '상태코드', example: 400},
+            message: {type: 'string', description: '메시지', example: ['실패메시지1', '실패메시지2']}
+        }
+    }})
     async typeorm(): Promise<object> {
         let result = await this.service.typeorm();
         return {
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { MainModule } from './module/main.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
 
 async function bootstrap() {
     dotenv.config();
     const app = await NestFactory.create(MainModule);
+
+
+    // Swagger
+    const config = new DocumentBuilder()
+    .setTitle('API Document')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
