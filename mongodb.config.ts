c2c8cb42f754ab6a3e62d80d839efa29477a6587
HaSeongJe <timeclas3@gmail.com>
2024-10-30 14:04:13 +0900

    2024.10.30
    - mongodb 커넥션 수정

--- mongodb.config.ts
+++ mongodb.config.ts
@@ -1,4 +1,15 @@
 import * as path from 'path';
 require('dotenv').config({path: path.resolve(__dirname, '../.env')});
 
-export default `mongodb://${process.env.MONGODB_URL}/${process.env.MONGODB_DB}`
+const path_items = ["mongodb://"];
+const ports = process.env.MONGODB_PORTS.split(",");
+if (ports.length > 1) {
+    for (let i=0; i<ports.length; i++) {
+        path_items.push(`${process.env.MONGODB_IP}:${ports[i]}${i+1 < ports.length ? "," : ""}`);
+    }
+    path_items.push(`/${process.env.MONGODB_DB}?replicaSet=${process.env.MONGODB_REPLICA_NAME}`);
+} else {
+    path_items.push(`${process.env.MONGODB_IP}:${ports[0]}/${process.env.MONGODB_DB}`)
+}
+
+export default path_items.join("");
