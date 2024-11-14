20de3ba1df1759e0ce9c67e9415295befaa4fa67
HaSeongJe <timeclas3@gmail.com>
2024-11-14 14:00:59 +0900

    2024.11.14
    - 회원존재여부를 interceptor에서 체크하도록 추가

--- src/interceptor/auth.interceptor.ts
+++ src/interceptor/auth.interceptor.ts
@@ -1,11 +1,16 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, HttpException, HttpStatus } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import * as util from '@util/util';
+import { DataSource } from 'typeorm';
 
-// 로그인시 허가
+// 관리자 허가
 @Injectable()
-export class LoginInterceptor implements NestInterceptor {
-    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+export class AdminInterceptor implements NestInterceptor {
+    constructor(
+        private readonly dataSource: DataSource
+    ) {}
+
+    async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
         const req = context.switchToHttp().getRequest();
         const token: string = req.headers['accesstoken'];
 
@@ -18,10 +23,28 @@ export class LoginInterceptor implements NestInterceptor {
                     throw new HttpException({code: 'EXPIRE_AUTH_TOKEN', message: '로그인 정보가 만료되었습니다.'}, HttpStatus.UNAUTHORIZED);
                 } else if (data['type'] !== 'access') {
                     throw new HttpException({code: 'FAIL_AUTH_TOKEN', message: '로그인 정보가 올바르지 않습니다.'}, HttpStatus.UNAUTHORIZED);
-                }
+                } else {
+                    const builder = this.dataSource.createQueryBuilder();
+                    builder.select(`
+                          u.user_seq
+                        , u.auth_code
+                        , u.user_name
+                        , u.user_nickname
+                        , u.user_mobile
+                    `)
+                    builder.from(`t_user`, 'u');
+                    builder.where('u.user_seq = :user_seq', {user_seq: data.user_seq});
+                    builder.andWhere(`u.use_yn = 'Y'`);
+                    const user = await builder.getRawOne();
+                    if (!user) {
+                        throw new HttpException({code: 'NO_SEARCH_USER', message: '회원 정보를 찾을 수 없습니다.'}, HttpStatus.BAD_REQUEST);
+                    } else if (!['ADMIN', 'SUPER_ADMIN'].includes(user['auth_code'])) {
+                        throw new HttpException({statusCode: 403, message: '권한이 없습니다.'}, HttpStatus.FORBIDDEN);
+                    }
 
-                req.headers.user_info = data;
-                return next.handle();
+                    req.headers.user_info = user;
+                    return next.handle();
+                }
             } else {
                 throw new HttpException({code: 'FAIL_AUTH_TOKEN', message: '로그인 정보가 올바르지 않습니다.'}, HttpStatus.UNAUTHORIZED);
             }
