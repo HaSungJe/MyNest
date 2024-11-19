401de2d1d5fb252361499396a17fa2166aa180e4
HaSeongJe <timeclas3@gmail.com>
2024-11-19 16:02:32 +0900

    2024.11.19
    - 쿼리 예외추가

--- src/util/errorFilter.ts
+++ src/util/errorFilter.ts
@@ -2,28 +2,32 @@ import { DataSource } from 'typeorm';
 
 /**
  * 에러메시지 필터
- * - QueryError
- * - Validation
  * 
- * @param errno 
- * @param err 
+ * @param dataSource 
+ * @param error 
+ * @returns 
  */
-export async function errMessageFilter(dataSource: DataSource, err: any) {
-    if (err.errno === 1062) { // 중복값
-        let msg = (err.sqlMessage.substring(err.sqlMessage.indexOf('key') + 5));
-        let params = (msg.substring(0, msg.length-1)).split('.');
-        let memo = await getColumnMemo(dataSource, params[0], params[1]);
+export async function errMessageFilter(dataSource: DataSource, error: any) {
+    if (error.errno === 1062) { // 중복값
+        const msg = (error.sqlMessage.substring(error.sqlMessage.indexOf('key') + 5));
+        const params = (msg.substring(0, msg.length-1)).split('.');
+        const memo = await getColumnMemo(dataSource, params[0], params[1]);
         return `이미 사용중인 ${memo} 입니다.`;
-    } else if (err.errno === 1048) { // 공백
-        let column_name = err.sqlMessage.substring(err.sqlMessage.indexOf("'") + 1, err.sqlMessage.lastIndexOf("'"));
-        let memo = await getColumnMemo2(dataSource, column_name);
+    } else if (error.errno === 1364) {
+        const split_error_message = error.sqlMessage.split(`'`);
+        const column_name = split_error_message[1];
+        const memo = await getColumnMemo2(dataSource, column_name);
+        return `${memo} (을)를 입력해주세요.`;
+    } else if (error.errno === 1048) { // 공백
+        const column_name = error.sqlMessage.substring(error.sqlMessage.indexOf("'") + 1, error.sqlMessage.lastIndexOf("'"));
+        const memo = await getColumnMemo2(dataSource, column_name);
         return `${memo}(을)를 입력해주세요.`;
-    } else if (err.errno === 1452) { // 올바르지 않은 외래키 입력
-        let msg = err.sqlMessage.substring(err.sqlMessage.indexOf('constraint fails') + 18, err.sqlMessage.indexOf('CONSTRAINT') - 2).replaceAll("`", "");
-        let table = msg.split('.');
-        let table_name = table[1];
-        let index_name = err.sqlMessage.substring(err.sqlMessage.indexOf('CONSTRAINT') + 12, err.sqlMessage.indexOf('FOREIGN KEY') - 2);
-        let memo = await getColumnMemo(dataSource, table_name, index_name);
+    } else if (error.errno === 1452) { // 올바르지 않은 외래키 입력
+        const msg = error.sqlMessage.substring(error.sqlMessage.indexOf('constraint fails') + 18, error.sqlMessage.indexOf('CONSTRAINT') - 2).replaceAll("`", "");
+        const table = msg.split('.');
+        const table_name = table[1];
+        const index_name = error.sqlMessage.substring(error.sqlMessage.indexOf('CONSTRAINT') + 12, error.sqlMessage.indexOf('FOREIGN KEY') - 2);
+        const memo = await getColumnMemo(dataSource, table_name, index_name);
         return `올바르지 않은 ${memo} 입니다.`;
     } else {
         return '실패하였습니다.';
@@ -39,17 +43,17 @@ export async function errMessageFilter(dataSource: DataSource, err: any) {
  * @returns 
  */
 export async function getColumnMemo(dataSource: DataSource, table_name: string, index_name: string): Promise<string> {
-    let builder = dataSource.createQueryBuilder();
+    const builder = dataSource.createQueryBuilder();
     builder.select(['a.column_name']);
     builder.from('information_schema.statistics', 'a');
     builder.where('a.table_name = :table_name and a.index_name = :index_name', {table_name: table_name, index_name});
-    let col_name = (await builder.getRawOne())['COLUMN_NAME'];
+    const col_name = (await builder.getRawOne())['COLUMN_NAME'];
     
-    builder = dataSource.createQueryBuilder();
-    builder.select(['a.column_comment as memo']);
-    builder.from('INFORMATION_SCHEMA.COLUMNS', 'a');
-    builder.where('a.column_name = :column_name', {column_name: col_name});
-    return (await builder.getRawOne())['memo'];
+    const builder2 = dataSource.createQueryBuilder();
+    builder2.select(['a.column_comment as memo']);
+    builder2.from('INFORMATION_SCHEMA.COLUMNS', 'a');
+    builder2.where('a.column_name = :column_name', {column_name: col_name});
+    return (await builder2.getRawOne())['memo'];
 }
 
 /**
@@ -60,7 +64,7 @@ export async function getColumnMemo(dataSource: DataSource, table_name: string, 
  * @returns 
  */
 export async function getColumnMemo2(dataSource: DataSource, column_name: string): Promise<string> {
-    let builder = dataSource.createQueryBuilder();
+    const builder = dataSource.createQueryBuilder();
     builder.select(['a.column_comment as memo']);
     builder.from('INFORMATION_SCHEMA.COLUMNS', 'a');
     builder.where('a.column_name = :column_name', {column_name: column_name});
